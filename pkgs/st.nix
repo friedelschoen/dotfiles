a94d6c25f88e99bd498b3fd00debee07fc678212
Friedel Schön <derfriedmundschoen@gmail.com>
2024-07-22 00:17:07 +0200

    bumb st-version

--- pkgs/st.nix
+++ pkgs/st.nix
@@ -4,7 +4,7 @@ with pkgs; stdenv.mkDerivation rec {
   name = "st";
   src = fetchGit {
     url = "https://git.friedelschoen.io/suckless/st";
-    rev = "c9b37cad883f0e811f2a4f11ead78649c0474f73";
+    rev = "b878d53d99a3a7108107f2da3a856a90872ec6fd";
   };
 
   nativeBuildInputs = [
